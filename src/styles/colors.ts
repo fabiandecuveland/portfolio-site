5144645ab542cf398a11612313f781bf5d96d17d
Fabian de Cuveland <fabian_de_cuveland@web.de>
2020-03-22 18:50:46 +0100

    Styles: add colors and use styled-normalize

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  licorice: '#16100c',
+  blackOlive: '#3A3735',
+  eggshell: '#e2efe1',
+  quickSilver: '#B5A7A2',
+  lightMossGreen: '#B8EdA8',
+};
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+import { colors } from 'styles/index';
 
 export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
   body {
-  
+    background-color: ${colors.licorice};
+    color: ${colors.eggshell};
+    text-align: center;
   }
 `;
