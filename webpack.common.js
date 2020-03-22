6084681a12d35360bbdea7227993e767d1310819
Fabian de Cuveland <fabian_de_cuveland@web.de>
2020-03-22 13:32:05 +0100

    Webpack: correctly resolve ts and tsx files

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx', 'ts', 'tsx'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
