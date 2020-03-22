c0eb9aff88c517507363bdd5d978f8bc1e5887f2
Fabian de Cuveland <fabian_de_cuveland@web.de>
2020-03-22 13:32:47 +0100

    Build: add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { GlobalStyle } from './styles/global';
 import './styles.scss';
 
 ReactDOM.render(
-  <div>{process.env.NODE_ENV}</div>,
+  <>
+    <GlobalStyle />
+    <div>{process.env.NODE_ENV}</div>
+  </>,
   document.getElementById('app')
 );
 
